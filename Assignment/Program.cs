d7ab2cf0916630fb0013a5cd13422d96f89f13f7
Naglaa <nagla2fat7y120@gmail.com>
2025-02-10 01:59:48 +0200

    All

---
+++ Assignment/Program.cs
@@ -0,0 +1,288 @@
+﻿using Day_01_G01;
+using static Day_01_G01.ListGenerator;
+
+namespace Assignment
+{
+    class CustomComparer : IComparer<string>
+    {
+        public int Compare(string? x, string? y)
+        {
+            return x.ToLower().CompareTo(y.ToLower());
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region LINQ - Restriction Operators
+
+            #region Question01
+
+            //var Result = ProductList.Where(P => P.UnitsInStock == 0);
+
+            //Result= from P in ProductList
+            //        where P.UnitsInStock == 0
+            //        select P;
+
+            //foreach (var item in Result)
+            //    Console.WriteLine(item);
+
+            #endregion
+
+            #region Question02
+
+            //var Result = ProductList.Where(P => P.UnitsInStock == 0 && P.UnitPrice > 3.00M);
+
+            //Result = from P in ProductList
+            //         where P.UnitsInStock == 0 && P.UnitPrice > 3.00M
+            //         select P;
+
+            //foreach (var item in Result)
+            //    Console.WriteLine(item);
+
+            #endregion
+
+            #region Question03
+
+            //string[] Arr = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+            //var Result = Arr.Where((P, I) => I < Arr.Length && P.Length < I);
+
+            //foreach (var item in Result)
+            //    Console.WriteLine(item);
+
+            #endregion
+
+            #endregion
+
+            #region LINQ - Ordering Operators
+
+            #region Question01
+
+            //var Result = ProductList.OrderBy(P => P.ProductName);
+
+            //    Result = from P in ProductList
+            //             orderby P.ProductName
+            //             select P;
+
+            //foreach (var item in Result)
+            //    Console.WriteLine(item);
+
+
+            #endregion
+
+            #region Question02
+
+            //string[] Arr = { "aPPLE", "AbAcUs", "bRaNcH", "BlUeBeRrY", "ClOvEr", "cHeRry" };
+
+            //var Result = Arr.Order(new CustomComparer());
+
+            //    Result= from P in Arr
+            //            orderby P.ToLower()
+            //            select P;
+
+
+            //foreach (var item in Result)
+            //    Console.WriteLine(item);
+
+            #endregion
+
+            #region Question03
+
+            //var Result = ProductList.OrderByDescending(P => P.UnitsInStock);
+
+            //Result = from P in ProductList
+            //         orderby P.UnitsInStock descending
+            //         select P;
+
+            //foreach (var item in Result)
+            //    Console.WriteLine(item);
+
+            #endregion
+
+            #region Question04
+
+            //string[] Arr = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+            //var Result = Arr.OrderBy(P => P.Length).ThenBy(P => P);
+
+            //    Result = from P in Arr
+            //             orderby P.Length, P
+            //             select P;
+
+
+            //foreach (var item in Result)
+            //    Console.WriteLine(item);
+
+            #endregion
+
+            #region Question05
+
+            //string[] words = { "aPPLE", "AbAcUs", "bRaNcH", "BlUeBeRrY", "ClOvEr", "cHeRry" };
+
+            //var Result = words.OrderBy(P => P.Length).ThenBy(P => P, new CustomComparer());
+
+            //    Result = from P in words
+            //             orderby P.Length, P.ToLower()
+            //             select P;
+
+            //foreach (var item in Result)
+            //    Console.WriteLine(item);
+
+            #endregion
+
+            #region Question06
+
+            //var Result = ProductList.OrderBy(P => P.Category).ThenByDescending(P => P.UnitPrice);
+
+            //Result = from P in ProductList
+            //         orderby P.Category, P.UnitPrice descending
+            //         select P;
+
+            //foreach (var item in Result)
+            //    Console.WriteLine(item);
+
+            #endregion
+
+            #region Question07
+
+            //string[] Arr = { "aPPLE", "AbAcUs", "bRaNcH", "BlUeBeRrY", "ClOvEr", "cHeRry" };
+
+            //var Result = Arr.OrderBy(P => P.Length).ThenByDescending(P => P, new CustomComparer());
+
+            //Result = from P in Arr
+            //         orderby P.Length, P.ToLower() descending
+            //         select P;
+
+            //foreach (var item in Result)
+            //    Console.WriteLine(item);
+
+            #endregion
+
+            #region Question08
+
+            //string[] Arr = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+            //var Result = Arr.Where(P => P[1]=='i').Reverse();
+
+            //foreach (var item in Result)
+            //    Console.WriteLine(item);
+
+            #endregion
+
+            #endregion
+
+            #region LINQ – Transformation Operators
+
+            #region Question01
+
+            //var Result = ProductList.Select((P) => P.ProductName);
+
+            //    Result = from P in ProductList
+            //             select P.ProductName;
+
+            //foreach (var item in Result)
+            //    Console.WriteLine(item);
+            #endregion
+
+            #region Question02
+
+            //string[] words = { "aPPLE", "BlUeBeRrY", "cHeRry" };
+
+            //var Result = words.Select(C => new { Lower = C.ToLower(), Upper = C.ToUpper() });
+
+            //Result = from C in words
+            //         select new { Lower = C.ToLower(), Upper = C.ToUpper() };
+
+            //foreach (var item in Result)
+            //    Console.WriteLine(item);
+
+            #endregion
+
+            #region Question03
+
+            //var Result = ProductList.Select(P => new
+            //                        {
+            //                            ID = P.ProductID,
+            //                            Name = P.ProductName,
+            //                            Price = P.UnitPrice 
+            //                        });
+
+            //Result = from P in ProductList
+            //         select new
+            //         {
+            //             ID = P.ProductID,
+            //             Name = P.ProductName,
+            //             Price = P.UnitPrice
+            //         };
+
+            //foreach (var item in Result)
+            //    Console.WriteLine(item);
+
+
+            #endregion
+
+            #region Question04
+
+            //int[] Arr = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+
+            //var Result = Arr.Select((P, I) => P == I);
+
+            //foreach (var item in Result)
+            //    Console.WriteLine(item);
+
+            #endregion
+
+            #region Question05
+
+            //int[] numbersA = { 0, 2, 4, 5, 6, 8, 9 };
+            //int[] numbersB = { 1, 3, 5, 7, 8 };
+
+            //var Result = numbersA.SelectMany((C) => numbersB, (C,D) => new { C, D }).Where(Result=>Result.C < Result.D);
+
+            //Result =from C in numbersA
+            //        from D in numbersB
+            //        where C<D
+            //        select new { C, D };
+
+            //foreach (var item in Result)
+            //    Console.WriteLine($"{item.C} is less than {item.D}");
+
+            #endregion
+
+            #region Question06
+
+            //var Result = CustomerList.SelectMany((C) => C.Orders, (C, order) => new { C, order })
+            //                         .Where(Result => Result.order.Total <500.00M);
+
+            //Result = from C in CustomerList
+            //         from order in C.Orders
+            //         where order.Total < 500.00M
+            //         select new { C, order };
+
+            //foreach (var item in Result)
+            //    Console.WriteLine(item);
+
+            #endregion
+
+            #region Question07
+
+            //var Result = CustomerList.SelectMany((C) => C.Orders, (C, order) => new { C, order })
+            //                         .Where(Result => Result.order.OrderDate.Year <= 1998);
+
+            //Result = from C in CustomerList
+            //         from order in C.Orders
+            //         where order.OrderDate.Year <= 1998
+            //         select new { C, order };
+
+            //foreach (var item in Result)
+            //    Console.WriteLine(item);
+
+            #endregion
+
+            #endregion
+
+        }
+    }
+}
---
+++ Demo/Program.cs
@@ -0,0 +1,212 @@
+﻿using Day_01_G01;
+using static Day_01_G01.ListGenerator;
+
+namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Var Vs Dynamic
+            #region Var
+            // var X = "Ahmed";
+
+            //// X = 10;  Not Valid 
+            #endregion
+
+            #region Dynamic
+            //dynamic X = "Ahmed";
+
+            //X = 10;
+            //X = 3.3; 
+            #endregion
+            #endregion
+
+            #region Extension Method
+            //int X = 12345;
+            //int Y=IntExtensions.Reverse(X);
+
+            //Y=X.Reverse();
+
+            //Console.WriteLine(Y); 
+            #endregion
+
+            #region AnonymousType
+            //    var employee = new { Id = 10, Name = "Ali", Salary = 6000 }; //AnonymousType0`3
+            //    Console.WriteLine(employee.GetType().Name);
+
+            //    var employee02 = new { Id = 20, Name = "Aya", Salary = 8000 }; //AnonymousType1`3
+
+            //    // employee.Id = 30;  Invalid
+            //    employee = new { Id = 30, employee.Name, employee.Salary };
+            //    employee = employee with { Id = 30 };  // Syntax Sugar C# 10.0 Feature 
+
+            //    var Product = new { ProductName = "Meat", Price = 120 }; // AnonymousType2`2
+            //} 
+            #endregion
+
+            #region What is LINQ
+            //// LINQ : +40 Extension Methods to the Built-in Interface "IEnumerable<T>"
+            ////      : Named as "LINQ Operators" Existed in class "Enumerable"
+            ////      : Categorized in 13 Category
+
+            //// Use "LINQ Operators" against Data (Stored in Sequence), Regardless DataBase Provider (SQL Server,MySQL,...)
+            //// Sequence: the object from class Implementing "IEnumerable<T>" (Collections: List,Array,...)
+            //// 1. Local Sequence
+            //// 2. Remote Sequence
+
+            //List<int> Numbers=new List<int>() {1,2,3,4,5,6,7,8,9 };
+            //List<int> OddNumbers = Numbers.Where((N)=>N%2==1).ToList(); 
+            #endregion
+
+            #region LINQ Syntax
+
+            // List<int> Numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            #region Fluent Syntax
+            //// 1.1 Call "LINQ Operator" as => Static Method through "Enumerable" Class
+            //var Odd = Enumerable.Where(Numbers, (N) => N % 2== 1);
+
+            //// 1.1 Call "LINQ Operator" as => Extension Method through "Recommended"
+            // Odd = Numbers.Where((N) => N % 2 == 1);
+
+            #endregion
+
+            #region Query Syntax
+            /// Starting with Keyword "from"
+            /// Introducing Range Variable (N): Represent each and every Element in the input Sequence
+            /// Ending with Keyword "select" OR "group by"
+
+            //var odd=from N in Numbers
+            //        where N%2==1
+            //        select N; 
+            #endregion
+
+            #endregion
+
+            #region LINQ Execution Ways
+            #region Differed Executio
+            //// 1. Differed Execution (Latest Version of Data)
+            //List<int> Numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            //var Result = Numbers.Where((N) =>N% 2 == 1);
+            //Numbers.AddRange(new int[] { 11, 12, 13, });
+
+            //foreach (int item in Result)
+            //    Console.WriteLine(item); 
+            #endregion
+
+            #region Immediate Execution
+            //// 1. Immediate Execution (Element Operators, Casting Operators, Aggregate Operators)
+            //List<int> Numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            //var Result = Numbers.Where((N) => N % 2 == 1).ToList();
+            //Numbers.AddRange(new int[] { 11, 12, 13, });
+
+            //foreach (int item in Result)
+            //    Console.WriteLine(item); 
+            #endregion
+            #endregion
+
+            #region Flteration (Restrection) Operators - Where, OfType
+            // First Overload
+            //var Result = ProductList.Where((P) => P.UnitsInStock == 0);
+            //Result= from P in ProductList
+            //        where P.UnitsInStock == 0
+            //        select P;
+
+            //var Result = ProductList.Where((P) => P.UnitsInStock == 0 && P.Category=="Meat/Poultry");
+            //Result = from P in ProductList
+            //         where P.UnitsInStock == 0 && P.Category == "Meat/Poultry"
+            //         select P;
+
+            // Second Overload Indexed Where 
+            // Valid Only in Fluency Syntax
+            // var Result = ProductList.Where((P,I) => I<10 && P.UnitsInStock == 0);
+
+            //OfType
+            // var Result = ProductList.OfType<Product02>();
+            #endregion
+
+            #region Transformation (Projection) Operators - Select,SelectMany
+
+            #region Select
+            //// First Overload
+            //var Result = ProductList.Select((P) => P.ProductName);
+            //Result= from P in ProductList
+            //        select P.ProductName;
+
+            //// Second Overload
+            //Result = ProductList.Where((P) => P.UnitsInStock == 0)
+            //                   .Select((P, I) => $"{I} : {P.ProductName}");
+
+
+            ///////////////
+
+            //var Result = ProductList.Where((P) => P.UnitsInStock > 0)
+            //                        .Select(P => new
+            //                        {
+            //                            ID = P.ProductID,
+            //                            Name = P.ProductName,
+            //                            Price = P.UnitPrice - (P.UnitPrice * .1M)
+            //                        });
+            //Result = from P in ProductList
+            //         where P.UnitsInStock > 0
+            //         select new
+            //         {
+            //             ID = P.ProductID,
+            //             Name = P.ProductName,
+            //             Price = P.UnitPrice - (P.UnitPrice * .1M)
+            //         };
+
+            #endregion
+
+            #region SelectMany
+            //var Result01 = CustomerList.SelectMany((C) => C.Orders);
+            //var Result02 = CustomerList.SelectMany((C,I) => C.Orders);
+           // var Result03 = CustomerList.SelectMany((C) => C.Orders,(C,Order)=>new {C,Order});
+            //var Result04 = CustomerList.SelectMany((C,I) => C.Orders, (C, Order) => new { C, Order });
+
+            //Result01 = from C in CustomerList
+            //           from O in C.Orders
+            //           select O;
+
+            //Result03 = from C in CustomerList
+            //           from Order in C.Orders
+            //           select new { C, Order }; 
+            #endregion
+
+            #endregion
+
+            #region Ordering Operators
+
+            //var Result01 = ProductList.Order();
+            //var Result02 = ProductList.OrderDescending();
+            //var Result03 = ProductList.OrderBy(P => P.UnitsInStock);
+            //var Result04 = ProductList.OrderByDescending(P => P.UnitsInStock);
+            //var Result05 = ProductList.OrderBy(P => P.UnitsInStock).ThenBy(P=>P.UnitPrice);
+            //var Result06 = ProductList.OrderByDescending(P => P.UnitsInStock).ThenByDescending(P => P.UnitPrice);
+
+            //Result03 = from P in ProductList
+            //             orderby P.UnitsInStock
+            //             select P;
+
+            //Result04 = from P in ProductList
+            //           orderby P.UnitsInStock descending
+            //           select P;
+
+            //Result05 = from P in ProductList
+            //           orderby P.UnitsInStock,P.UnitPrice
+            //           select P;
+
+            //Result06 = from P in ProductList
+            //           orderby P.UnitsInStock descending, P.UnitPrice descending
+            //           select P;
+
+            //// Fluency Syntax Only
+            //var Result07 = ProductList.Where(P => P.UnitsInStock == 0).Reverse();
+            #endregion
+
+        }
+    }
+}
